460c7dbcd548ab07cacbaa1ad5d4a4bf945f2c3d
tharick <tharick@mail.usf.edu>
2019-01-15 17:58:44 -0500

    HelloName 2.0

--- HelloName/Program.cs
+++ HelloName/Program.cs
@@ -5,8 +5,14 @@ namespace HelloName
     class Program
     {
         static void Main(string[] args)
+           
         {
-            Console.WriteLine("Hello World!");
+            string name = ("Tharick Moulton");
+            Console.WriteLine(name);
+            Console.WriteLine("Press any key to continue");
+           
+            Console.ReadKey(true);
+
         }
     }
 }
